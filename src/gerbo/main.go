0b352b009f6de8fa1c40d0c914f8285cd5c77ef3
Lucas Alves <luk4z_7@hotmail.com>
2017-07-24 23:22:43 -0300

    Refactoring main.go
    	* Included Mutex, and executing one single goroutine.

--- src/gerbo/main.go
+++ src/gerbo/main.go
@@ -8,12 +8,33 @@ import (
 	"gerbo/lib/logs"
 	"gerbo/controllers/operation"
 	"runtime"
+	"gerbo/core/drive/sqlite"
+	"database/sql"
+	"sync"
+	"time"
 )
+
+type Instance struct {
+	sync.Mutex
+	db *sql.DB
+}
 
 func main() {
 	logs.Start()
 	logs.INFO.Println("Running!")
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	operation.Start()
+	instance := Instance{}
+	instance.db = sqlite.GetDB()
+	defer instance.db.Close()
+
+	done := make(chan struct{})
+	go func() {
+		done <- operation.Start(instance.db)
+	}()
+	time.Sleep(time.Second)
+	instance.Lock()
+	defer instance.Unlock()
+
+	<-done
 }
